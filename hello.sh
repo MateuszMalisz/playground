3719920cdf4631da1f28689981da28bd06cfcdcd
Mateusz Malisz <maliszmat@outlook.com>
2020-12-14 01:26:24 -0800

    Update hello

--- hello.sh
+++ hello.sh
@@ -4,7 +4,11 @@ countFile="count"
 # Get count
 runCount=`cat $countFile`
 # Print
-echo "Hello! You are running this $runCount time!"
+echo "Hello! You are running this $runCount. time!"
 # Save updated count
 runCount=$((runCount+1))
 echo "$runCount" > "$countFile"
+
+git add count
+git commit -m "Bump #$runCount"
+git push origin master
