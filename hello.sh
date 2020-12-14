b52a9958aff585bbe6aa8a2975fc4697583af45e
Mateusz Malisz <maliszmat@outlook.com>
2020-12-14 02:26:25 -0800

    Automate hello even more

--- hello.sh
+++ hello.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# $1 is gh token
+# $2 is git token
+
 countFile="count"
 # Get count
 runCount=`cat $countFile`
@@ -9,6 +12,19 @@ echo "Hello! You are running this $runCount. time!"
 runCount=$((runCount+1))
 echo "$runCount" > "$countFile"
 
+https://github.com/cli/cli/releases/download/v1.3.1/gh_1.3.1_linux_arm64.tar.gz
+tar xf gh*
+PATH="$PATH:$PWD/gh_1.3.1_linux_arm64/bin"
+
+# Masquerade as Mateusz
+git config --local user.name MateuszMalisz
+git config --local user.email mamalisz@microsoft.com
+git config --local credential.helper "store --file $2"
+
 git add count
-git commit -m "Bump #$runCount"
-git push origin master
+git commit -m "Updating count to $runCount"
+git push origin first
+
+gh config set prompt disabled
+gh auth login --with-token < $1
+gh pr create --title "[I LIKE TRAINS] ${runCount}. train! " --base master --head first --reviewer MateuszMalisz --body "(sounds of trains...)"
